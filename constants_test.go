ddd868927ccac4ccf5bb7e3b6c7b6aa27e089a4a
Luke <freeman.luke@gmail.com>
2019-04-06 14:52:47 -0700

    tidying

--- constants_test.go
+++ constants_test.go
@@ -1,3 +1,12 @@
+// Goro
+//
+// Created by Yakka
+// http://theyakka.com
+//
+// Copyright (c) 2019 Yakka LLC.
+// All rights reserved.
+// See the LICENSE file for licensing details and requirements.
+
 package goro_test
 
 const printDebug = true
